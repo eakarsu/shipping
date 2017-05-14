8b74b96b09fb64e58bf8822a36c89986ec50a040
Erol Akarsu <eakarsu@gmail.com>
2017-05-13 22:32:32 -0400

    first processing one user tracking numbers: login, fetching tracking report from fedex, process all track nums to find any eligible transaction

--- fedexUtils.js
+++ fedexUtils.js
@@ -14,6 +14,9 @@ var rp = require('request-promise');
 var Promise = require("bluebird");
 var cheerio = require("cheerio");
 var jp = require('jsonpath');
+var extract = require('extract-zip')
+var csvparse =  require('csv-parse/lib/sync');
+
 var fedexURL = "https://www.fedex.com/fcl/logon.do";
 let fedexDownloadUrl = "https://www.fedex.com/reporting/DownloadPage.do";
 let fedexDownloadPrepareUrl = "https://www.fedex.com/reporting/StandardReports.do";
@@ -57,8 +60,7 @@ let downloadform = {
     "standardReport":"217",
     "reportBaseType":"payer",
     "accountType":"standardfedexaccounts",
-    "selectedAccountNumbers":"235101130",
-    "selectedAccountNumbers":"721459721",
+    "selectedAccountNumbers":["235101130","721459721"],
     "selectedFilterColumn":"Show All",
     "filterValueForSelectedColumn":"",
     "StartWeek":"Start week",
@@ -66,7 +68,7 @@ let downloadform = {
     "StartMonth":"Start month",
     "EndMonth":"End month",
     "reportOccurrence":"onetime",
-    "onetime_reportview":"monthly",
+    "onetime_reportview":"weekly",
     "onetimestarting_month":"4",
     "onetimestarting_year":"2017",
     "reportDateStart":"4/23/2017",
@@ -81,9 +83,14 @@ let csvDownload = {
     "riID":"5065426"
 }
 
+let oneTrackReq = {"TrackPackagesRequest":{"appType":"WTRK","appDeviceType":"DESKTOP","uniqueKey":"","processingParameters":{},
+    "trackingInfoList":[
+        {"trackNumberInfo":{"trackingNumber":"{trackingNumber}","trackingQualifier":"","trackingCarrier":""}}
+        ]
+}};
 
 let trackPostFrom = {
-    "data":'{"TrackPackagesRequest":{"appType":"WTRK","appDeviceType":"DESKTOP","uniqueKey":"","processingParameters":{},"trackingInfoList":[{"trackNumberInfo":{"trackingNumber":"{trackingNumber}","trackingQualifier":"","trackingCarrier":""}}]}}',
+    "data":"",
     "action":"trackpackages",
     "locale":"en_US",
     "version":"1",
@@ -92,17 +99,148 @@ let trackPostFrom = {
 
 let cookiesCache = {};
 
+processOneUser ("mtahardware1","","");
 
-let promise = getEstimatedDeliveryDateFromPost("778854424040");//"778811840479");//"778854424040");//"731571689223"); //"779058840541");
-promise.then (x=>{
-    console.log (JSON.stringify(x));
-})
-
-//reportDownloadProcess();
-
-function reportDownloadProcess ()
+function processOneUser (userName,userPassword,accountNumber)
 {
-    let loginPromise = fedexLogin("","1907Fb1905Gs");
+    /*
+    let loginPromise = fedexLogin(userName,userPassword);
+    let newDownloadForm = prepareDownloadInputJson(accountNumber);
+    let promise2 = downloadDocument (loginPromise,newDownloadForm);
+    */
+    let promise2 = Promise.try(function(){
+        return "ok";
+    });
+
+    let promise3 = unzipReport(promise2);
+    let trackNumbersPromise =    collectTrackNumbers (promise3);
+    let truckNumPromises = processAllTrackingNums(trackNumbersPromise);
+    Promise.all(truckNumPromises).then (result =>{
+        console.log ("processed all:"+JSON.stringify(result));
+        let refunds = result.filter(x=>x && x.isRefundEligible);
+        console.log (`Obtained ${refunds.length} refunds`);
+        ref
+    })
+}
+
+function processAllTrackingNums (trackNumsPromise)
+{
+    return  trackNumsPromise.then (trackingNumbers => {
+        return  Promise.try(function(){
+            return trackingNumbers.map (tnum =>{
+            let list = [{"trackNumberInfo":{"trackingNumber":tnum,"trackingQualifier":"","trackingCarrier":""}}];
+            let newTrackPostFrom =  JSON.parse(JSON.stringify(trackPostFrom));
+            let newOneTrackReq = JSON.parse(JSON.stringify(oneTrackReq));
+            newOneTrackReq.TrackPackagesRequest.trackingInfoList = list;
+            newTrackPostFrom.data = JSON.stringify(newOneTrackReq);
+            let result = getEstimatedDeliveryOfBlockNums (newTrackPostFrom);
+            return result.then (jsonRes => {
+                console.log ("Processing this record:"+JSON.stringify(jsonRes));
+                return processOneRec (jsonRes);
+            })
+        })});
+
+
+    });
+}
+
+function processOneRec (oneTrackRecord) {
+    let nodes = jp.nodes(oneTrackRecord, "$..*[?(@.isException == true)].status")
+    let isException = nodes.length > 0;
+    //If there is exception, disregard the processing this tracking result.
+    if (isException) {
+        console.log("This tracking record has expception :" + oneTrackRecord);
+        return;
+    }
+    let transitType = jp.query(oneTrackRecord, "$..trackingCarrierDesc")[0];
+    if (transitType.match(/Fedex Ground/)) {
+        console.log("Fedex Ground")
+    } else if (transitType.match(/Express/)) {
+        console.log("Fedex Express")
+    }
+    let trackingNum = jp.query(oneTrackRecord, "$..trackingNbr")[0];
+    let expectedTime = jp.query(oneTrackRecord, "$..stdTransitTimeEnd")[0];
+    let expectedDate = jp.query(oneTrackRecord, "$..displayStdTransitDate")[0];
+    let deliveryTime = jp.query(oneTrackRecord, "$..actDeliveryDt")[0];
+    let deliveryDate = jp.query(oneTrackRecord, "$..displayActDeliveryDt")[0];
+    let shippedTime = jp.query(oneTrackRecord, "$..shipDt")[0];
+    //For ground, ignore time delivered. If it is delivered in same date, then it is ok
+    //Got non-ground, take day and time, calculate eligiblity
+    var isRefundEligible;
+    if (transitType.match(/Ground/))
+        isRefundEligible = new Date(deliveryDate).getTime() > new Date(expectedDate).getTime();
+    else
+        isRefundEligible = new Date(deliveryTime).getTime() > new Date(expectedTime).getTime();
+    let result =
+    {
+        trackingNum: trackingNum,
+        expectedTime: expectedTime,
+        deliveryTime: deliveryTime,
+        shippedTime: shippedTime,
+        isRefundEligible: isRefundEligible,
+        transitType:transitType
+    };
+    return Promise.try(function(){
+        return result
+    });
+}
+
+function unzipReport (prevPromise)
+{
+    return prevPromise.then(x=> {
+        let fn = "tmp/report.zip";
+        let promiseExract = Promise.promisify(extract);
+        let promiseObj = promiseExract(fn, {dir: "/tmp"});
+        return promiseObj;
+    });
+}
+
+function prepareDownloadInputJson (accountNumber,startDate,endDate)
+{
+    //downloadform
+    var now = new Date();
+    var today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
+
+    //regard last Sunday as today
+    today = new Date(today.setDate(today.getDate()-today.getDay()-1));
+    var oneWeekBefore = new Date(today.setDate(today.getDate()-today.getDay()-1));
+    oneWeekBefore.setDate(today.getDate() - 6);
+    if (startDate)
+        oneWeekBefore = startDate;
+    if (endDate)
+        today = endDate;
+    downloadform["selectedAccountNumbers"] = accountNumber;
+    downloadform.onetimeending_month = (today.getMonth()+1).toString();
+    downloadform.onetimestarting_month = (oneWeekBefore.getMonth()+1).toString();
+    downloadform.onetimeending_year = today.getFullYear().toString();
+    downloadform.onetimestarting_year =  oneWeekBefore.getFullYear().toString();
+    downloadform.reportDateEnd = today.toLocaleDateString();
+    downloadform.reportDateStart =  oneWeekBefore.toLocaleDateString();
+    return downloadform;
+}
+
+function collectTrackNumbers (promise)
+{
+    return promise.then (x=> {
+        let files = fs.readdirSync("/tmp");
+        let fn = files.filter(x=>x.match(/^FedEx_Shipment_Detail_Payer_Detail/))[0]
+        let contents = fs.readFileSync(`/tmp/${fn}`);
+        var records = csvparse(contents, {columns: true});
+        console.log("read and parsed csv file");
+        let trackingNumbers = records.map(x=> {
+            return x["Shipment Tracking Number"].trim();
+        });
+        console.log("tracking number=" + JSON.stringify(trackingNumbers));
+        return Promise.try(function(){
+            return trackingNumbers;
+        })
+    })
+}
+
+
+function reportDownloadProcess (loginPromise)
+{
+    //let loginPromise = fedexLogin("mtahardware1","1907Fb1905Gs");
     let fileDowloaded = downloadDocument(loginPromise);
     fileDowloaded.then (res=>{
         console.log ("I see file");
@@ -115,7 +253,8 @@ function fedexLogin(userName,userPaswd) {
     let optionsforPost = getOptionsForPost(loginform,fedexURL);
     return rp(optionsforPost)
 }
-function downloadDocument(loginPromise) {
+
+function downloadDocument(loginPromise,downloadform) {
 
     console.log("donwloadDocument");
     return loginPromise.then(res=>{
@@ -132,13 +271,14 @@ function downloadDocument(loginPromise) {
         options.Referer = "https://www.fedex.com/reporting/PrepareReport.do";
         return rp (options)
     }).then(res=>{
-        let uri = "https://www.fedex.com/reporting/download/report/fcro_report_CSV_5065426.zip";
+        let rid = res.body.match(/requestFileDownloadx\((\d+)/)[1];
+        let uri = `https://www.fedex.com/reporting/download/report/fcro_report_CSV_${rid}.zip`;
         let options =  getOptionsForPost(csvDownload,uri,res);
         options.encoding = "binary";
         return rp (options)
     }).then (res=> {
         console.log("download completed!!");
-        fs.writeFileSync("test.zip", res.body, 'binary');
+        fs.writeFileSync("tmp/report.zip", res.body, 'binary');
         return Promise.try(function(){
             return res.body;
         })
@@ -168,7 +308,7 @@ function getEstimatedDeliveryDateFromPost (trackingNumber)
     var uri = fedexTrackPostCall;
     let form = JSON.parse(JSON.stringify(trackPostFrom));
     form.data = form.data.replace (/\{trackingNumber\}/,trackingNumber);
-    let options =  getOptionsForPost(form,uri)
+    let options =  getOptionsForPost(form,uri);
 
     return rp(options).then (res=>{
         let sp = JSON.parse(res.body)
@@ -176,6 +316,22 @@ function getEstimatedDeliveryDateFromPost (trackingNumber)
         let estimatedDate = sp.TrackPackagesResponse.packageList[0].standardTransitDate.displayStdTransitDate;
         return Promise.try(function(){
             return {date:estimatedDate,time:estimatedTime};
+        })
+    })
+};
+
+function getEstimatedDeliveryOfBlockNums (trackingFormObject)
+{
+
+    let form = JSON.parse(JSON.stringify(trackingFormObject));
+    let options =  getOptionsForPost(form,fedexTrackPostCall);
+
+    return rp(options).then (res=>{
+        let sp = JSON.parse(res.body);
+        //let estimatedTime = sp.TrackPackagesResponse.packageList[0].standardTransitTimeWindow.displayStdTransitTimeEnd;
+        //let estimatedDate = sp.TrackPackagesResponse.packageList[0].standardTransitDate.displayStdTransitDate;
+        return Promise.try(function(){
+            return sp;
         })
     })
 };
